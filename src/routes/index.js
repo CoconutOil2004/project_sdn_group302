b85ca9b2d95d123b9c1cedca720e543cf3acd149
longdbhe186915 <longdbhe186915@fpt.edu.vn>
2025-10-17 16:36:19 +0700

    feat(notifications): implement notifications API endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const eventRouter = require("./events");
 const clubRouter = require("./clubs");
+const notificationRoutes = require("./notifications");
 
 const router = express.Router();
 router.use("/clubs", clubRouter);
 router.use("/events", eventRouter);
+router.use("/notifications", notificationRoutes);
 module.exports = router;
