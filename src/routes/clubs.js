453ad7ffa32add9ab8ac706c05f169ecfb8a9f61
HungTran <hungtd2k4@gmail.com>
2025-11-06 03:22:49 +0700

    add member to club, approve club

--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -3,10 +3,14 @@ const {
   getAllClubs,
   getClubDetailbyId,
   createClub,
+  approveClub,
+  addMemberToClub,
 } = require("../controllers/controllerClubs");
 
 const clubRouter = express.Router();
 clubRouter.get("/", getAllClubs);
 clubRouter.get("/:id", getClubDetailbyId);
-clubRouter.post("/", createClub)
+clubRouter.post("/", createClub);
+clubRouter.post("/", approveClub);
+clubRouter.post("/add-member", addMemberToClub);
 module.exports = clubRouter;
