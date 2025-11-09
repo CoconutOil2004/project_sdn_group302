e68af242bab2962cff305b401260a13c85e241e3
HungTran <hungtd2k4@gmail.com>
2025-11-09 22:09:47 +0700

    fix schema event

--- src/models/events.js
+++ src/models/events.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const EventSchema = new mongoose.Schema(
   {
     clubId: {
--- src/utils/Upload.js
+++ src/utils/Upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("../../config/cloudinary");
 
 // ⚙️ Cấu hình storage upload thẳng lên Cloudinary
 const storage = new CloudinaryStorage({
